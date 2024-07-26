b150a8540b2de3853467460156cea077a68e5172
Michael Bishop <cleverca22@gmail.com>
2024-07-26 20:56:36 -0300

    switch to my pr fork

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/staging";
+    nixpkgs.url = "github:cleverca22/nixpkgs/systemd-fixes";
     linux = {
       url = "github:rwf93/linux/xenon-6.5";
       flake = false;
