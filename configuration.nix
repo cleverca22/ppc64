bf3045a39a5b2ca0bde12cdb3c0d957741df6577
Michael Bishop <cleverca22@gmail.com>
2024-07-31 18:11:09 -0300

    back to startx

--- configuration.nix
+++ configuration.nix
@@ -85,8 +85,8 @@
       excludePackages = [ pkgs.xorg.xf86inputevdev.out ];
       desktopManager.xfce.enable = false;
       displayManager = {
-        lightdm.enable = true;
-        #startx.enable = true;
+        lightdm.enable = false;
+        startx.enable = true;
         #xserverBin = lib.mkForce "${pkgs.xorg.xvfb}/bin/Xvfb";
       };
       #videoDrivers = [];
