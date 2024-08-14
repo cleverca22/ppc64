e206d85c5c45760f473f891ad62874fa6b52e886
Michael Bishop <cleverca22@gmail.com>
2024-08-14 16:53:04 -0300

    build xell with nix

--- flake.nix
+++ flake.nix
@@ -5,12 +5,18 @@
       url = "github:rwf93/linux/xenon-6.5";
       flake = false;
     };
+    xell.url = "github:cleverca22/xell-reloaded";
+    xell.flake = false;
+    libxenon.url = "github:cleverca22/libxenon";
+    libxenon.flake = false;
+    libfat.url = "github:cleverca22/libfat";
+    libfat.flake = false;
   };
   nixConfig = {
     extra-substituters = [ "https://hydra.angeldsis.com/" ];
     extra-trusted-public-keys = [ "hydra.angeldsis.com-1:7s6tP5et6L8Y6sX7XGIwzX5bnLp00MtUQ/1C9t1IBGE=" ];
   };
-  outputs = { self, nixpkgs, linux }:
+  outputs = { self, nixpkgs, linux, xell, libxenon, libfat }:
   let
     host = import nixpkgs { system = "x86_64-linux"; };
     p = import nixpkgs {
@@ -18,8 +24,35 @@
       crossSystem = import ./cross.nix;
       overlays = [ (import ./overlay.nix) ];
     };
+    overlay32 = self: super: {
+      libxenon = self.callPackage libxenon {};
+      fat = self.callPackage libfat {};
+      xell2 = p.callPackage xell {
+        inherit (self) fat libxenon;
+        stdenv32 = self.stdenv;
+        stage = 2;
+      };
+      xell1 = p.callPackage xell {
+        inherit (self) fat xell2 libxenon;
+        stdenv32 = self.stdenv;
+        stage = 1;
+      };
+    };
+    pkgs32 = import nixpkgs {
+      system = "x86_64-linux";
+      crossSystem = {
+        config = "powerpc-none-eabi";
+        libc = "newlib";
+      };
+      overlays = [ overlay32 ];
+    };
   in {
     legacyPackages.powerpc64-linux = import nixpkgs { system = "powerpc64-linux"; overlays = [ (import ./overlay.nix) ]; };
+    packages = {
+      powerpc-none-eabi = {
+        inherit (pkgs32) libxenon fat xell1 xell2;
+      };
+    };
     packages.powerpc64-linux = {
       gccgo = p.buildPackages.gccgo.cc.overrideAttrs (old: {
         #outputs = [ "out" "man" "info" "lib" ];
@@ -71,10 +104,15 @@
     in {
       inherit (pkgs.xorg) xorgserver;
     };
-    hydraJobs.powerpc64-linux = {
-      inherit (self.packages.powerpc64-linux) nixos xterm xorgserver xvfb mesa;
-      inherit (p) lightdm sx xpra sddm i3 toxvpn;
-      inherit (p.gnome) gdm;
+    hydraJobs = {
+      powerpc64-linux = {
+        inherit (self.packages.powerpc64-linux) nixos xterm xorgserver xvfb mesa;
+        inherit (p) lightdm sx xpra sddm i3 toxvpn;
+        inherit (p.gnome) gdm;
+      };
+      powerpc-none-eabi = {
+        inherit (pkgs32) libxenon fat xell1 xell2;
+      };
     };
   };
 }
