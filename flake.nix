ce5f872a308fa28c2d642ba0470d6b0844c58b05
Michael Bishop <cleverca22@gmail.com>
2024-08-28 03:03:53 -0300

    remove sx

--- flake.nix
+++ flake.nix
@@ -176,7 +176,7 @@
       powerpc64-linux = {
         inherit (self.packages.powerpc64-linux) nixos xterm xorgserver xvfb mesa;
         #inherit (p) lightdm sx sddm toxvpn;
-        inherit (nativeppc64) i3 hello lightdm sx sddm toxvpn nix;
+        inherit (nativeppc64) i3 hello lightdm sddm toxvpn nix;
         qemu-user = p.qemu.override {
           alsaSupport = false;
           canokeySupport = false;
